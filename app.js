654ab4c8ed5e250079bc0ce13916bbe50ae0bd11
ZS <zsalik@gmail.com>
2019-04-24 00:33:41 -0400

    started app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+const game = () => {
+  let pScore = 0;
+  let cScore = 0;
+
+  //Start the game
+  const startGame = () => {
+    const playBtn = document.querySelector(".intro button");
+    const introScreen = document.querySelector(".intro");
+    const match = document.querySelector(".match");
+
+    playBtn.addEventListener("click", () => {
+      introScreen.classList.add(fadeOut);
+      match.classList.add("fadeIn");
+    });
+  };
+};
